abb3132dbd396e4721b16cc8f49434126a46f758
kgxfnio <95566842+kgxfnio@users.noreply.github.com>
2022-07-18 15:45:18 +0300

    Add files via upload

--- proj/tasks.js
+++ proj/tasks.js
@@ -1,25 +1,51 @@
-function calculateValuesPlus(){
-    let firstValue = document.getElementById('input1').value;
-    let secondValue = document.getElementById('input2').value;
-    let thirdValue =  document.getElementById('input3').value;
-    document.getElementById('result').innerText = Number(firstValue) + Number(secondValue) + Number(thirdValue);
+function calculateValues(){
+    
+    document.getElementById('RESULT').innerText = Number(firstValue) + Number(secondValue) + Number(thirdValue);
 
 }
 function calculateValuesMinus(){
     let firstValue = document.getElementById('input1').value;
     let secondValue = document.getElementById('input2').value;
     let thirdValue =  document.getElementById('input3').value;
-    document.getElementById('result').innerText = Number(firstValue) - Number(secondValue) - Number(thirdValue);
+    document.getElementById('RESULT').innerText = Number(firstValue) - Number(secondValue) - Number(thirdValue);
 }
 function calculateValuesDivide(){
     let firstValue = document.getElementById('input1').value;
     let secondValue = document.getElementById('input2').value;
     let thirdValue =  document.getElementById('input3').value;
-    document.getElementById('result').innerText = Number(firstValue) / Number(secondValue) / Number(thirdValue);
+    document.getElementById('RESULT').innerText = Number(firstValue) / Number(secondValue) / Number(thirdValue);
 }
 function calculateValuesMultiply(){
     let firstValue = document.getElementById('input1').value;
     let secondValue = document.getElementById('input2').value;
     let thirdValue =  document.getElementById('input3').value;
-    document.getElementById('result').innerText = Number(firstValue) * Number(secondValue) * Number(thirdValue);
+    document.getElementById('RESULT').innerText = Number(firstValue) *  Number(secondValue) * Number(thirdValue);
+}
+function getSelectValue(){
+    if(document.getElementById('operations').value == 'choose operation'){
+        alert('choose operation')
+    }
+    else{
+        let firstValue = document.getElementById('input1').value;
+        let secondValue = document.getElementById('input2').value;
+        let thirdValue =  document.getElementById('input3').value;
+        let value = document.getElementById('operations').value
+        switch(value){
+            case '+':
+                document.getElementById('RESULT').innerText = Number(firstValue) +  Number(secondValue) + Number(thirdValue);
+            break;
+            case '-':
+                document.getElementById('RESULT').innerText = Number(firstValue) -  Number(secondValue) - Number(thirdValue);
+            break;
+            case '/':
+                document.getElementById('RESULT').innerText = Number(firstValue) /  Number(secondValue) / Number(thirdValue);
+            break;
+            case '*':
+                document.getElementById('RESULT').innerText = Number(firstValue) *  Number(secondValue) * Number(thirdValue);
+            break;
+            default:
+                console.log('nigg')
+            break;
+        }
+    }
 }
